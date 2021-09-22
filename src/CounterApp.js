c5cd172079cbce8e53af3d03fd999e7538c843ff
Arsenio Jimenez <moises.jimenez92@gmail.com>
2021-09-22 16:15:13 -0300

    first commit

---
+++ src/CounterApp.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import PropTypes from 'prop-types';
+
+const CounterApp = ({ value }) => {
+
+    const [ counter, setCounter ] = useState(value);
+
+    //HandelAdd
+    const handleAdd = () => {
+
+        setCounter( counter + 1 );
+
+        //Se pueden retornar funciones que den como resultado el nuevo valor de la costante
+        /*setCounter ( (counter) => {
+            return counter + 1
+        } )*/
+    };
+
+    //handleSubstract
+    const handleSubstract = () => {
+        setCounter( counter - 1 );
+    }
+
+    //handleReset
+    const handleReset = () => {
+        setCounter( value );
+    }
+
+    return (
+        <>
+            <h1>CounterApp</h1>
+            <h2> {counter} </h2>
+
+
+            <button onClick={ handleAdd }> +1 </button>
+            <button onClick={ handleReset }> reset </button>
+            <button onClick={ handleSubstract }> -1 </button>
+
+
+        </>
+    )
+
+};
+
+CounterApp.propTypes = {
+    value: PropTypes.number
+};
+
+CounterApp.defaultProps = {
+    value: 0
+}
+
+
+export default CounterApp;
---
+++ src/PrimeraApp.js
@@ -0,0 +1,33 @@
+//Componentes funcionales
+import React from 'react';
+import PropTypes from 'prop-types';
+//import React, {Fragment} from 'react';
+
+//Agregar valores por defecto es una bueno practica
+//const PrimeraApp = ({ saludo = 'Hola Mundo' }) => {
+const PrimeraApp = ({ saludo, subtitulo }) => {
+
+
+    //Obligar a que vengan las props que necesito
+
+
+    return (
+        <>
+            <h1> { saludo } </h1>
+            <p> { subtitulo } </p>
+        </>
+    ); 
+    
+}
+
+//Obligar a que vengan las props que necesito
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string.isRequired
+}
+
+PrimeraApp.defaultProps = {
+    saludo: 'hola mundo',
+    subtitulo: 'soy un subtitulo 1234'
+}
+
+export default PrimeraApp;
